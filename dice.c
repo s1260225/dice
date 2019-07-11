241f5de879d407d5cf32651c9e32801f2b822046
s1260225 <52781717+s1260225@users.noreply.github.com>
2019-07-11 18:49:42 +0900

    Add files via upload

---
+++ dice.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int i;
+
+void dice(){
+  int DICE[2];
+  srand((unsigned int)time(NULL));
+  printf("Rolling the dice ...");
+  for(i = 0; i < 2; i++){
+    DICE[i] = rand()%6 + 1;
+    printf("Die %d: %d\n",i+1,DICE[i]);
+  }
+  printf("Total value :%-3d\n",DICE[0] + DICE[1]);
+}
+int main(){
+  dice();
+  return 0;
+}
